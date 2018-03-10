8dfbc6163b319a60e71672d614c4da52aaadf649
Alberto Miranda <alberto.miranda@bsc.es>
2018-03-10 20:40:54 +0100

    Update license headers

--- include/norns/norns_resources.h
+++ include/norns/norns_resources.h
@@ -1,25 +1,29 @@
-/* * Copyright (C) 2017-2018 Barcelona Supercomputing Center
- *                           Centro Nacional de Supercomputacion
- *
- * This file is part of the Data Scheduler, a daemon for tracking and managing
- * requests for asynchronous data transfer in a hierarchical storage environment.
- *
- * See AUTHORS file in the top level directory for information
- * regarding developers and contributors.
- *
- * The Data Scheduler is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Data Scheduler is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Data Scheduler.  If not, see <http://www.gnu.org/licenses/>.
- */
+/*************************************************************************
+ * Copyright (C) 2017-2018 Barcelona Supercomputing Center               *
+ *                         Centro Nacional de Supercomputacion           *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * This file is part of the NORNS Data Scheduler, a service that allows  *
+ * other programs to start, track and manage asynchronous transfers of   *
+ * data resources transfers requests between different storage backends. *
+ *                                                                       *
+ * See AUTHORS file in the top level directory for information           *
+ * regarding developers and contributors.                                *
+ *                                                                       *
+ * The NORNS Data Scheduler is free software: you can redistribute it    *
+ * and/or modify it under the terms of the GNU Lesser General Public     *
+ * License as published by the Free Software Foundation, either          *
+ * version 3 of the License, or (at your option) any later version.      *
+ *                                                                       *
+ * The NORNS Data Scheduler is distributed in the hope that it will be   *
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty   *
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  *
+ * Lesser General Public License for more details.                       *
+ *                                                                       *
+ * You should have received a copy of the GNU Lesser General             *
+ * Public License along with the NORNS Data Scheduler.  If not, see      *
+ * <http://www.gnu.org/licenses/>.                                       *
+ *************************************************************************/
 
 #ifndef __NORNS_RESOURCES_H__
 #define __NORNS_RESOURCES_H__ 1
