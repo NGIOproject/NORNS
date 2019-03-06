8f8849fa58b9fe434024a20e946827f378d1c39e
Alberto Miranda <alberto.miranda@bsc.es>
2019-03-06 22:38:08 +0100

    Update .gitlab-ci.yml

--- gencov.sh
+++ gencov.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 GIT_ROOTDIR=`git rev-parse --show-toplevel`
+LCOV=`which lcov`
 
-/usr/local/bin/lcov \
+${LCOV} \
     `find ${GIT_ROOTDIR} -name "*.gcda" 2>/dev/null | xargs -I{} dirname {} | uniq | xargs -I {} echo -n " --directory "{}` \
     --capture \
     --output-file gcov.info \
     2&>1 /dev/null
 
-/usr/local/bin/lcov \
+${LCOV} \
     --remove gcov.info \
         '/usr/include/*' \
         '/usr/local/include/*' \
@@ -19,6 +20,6 @@ GIT_ROOTDIR=`git rev-parse --show-toplevel`
     -o norns.info \
     2&>1 /dev/null
 
-/usr/local/bin/lcov -l norns.info
+${LCOV} -l norns.info
 
 #genhtml -o html/coverage norns.info 2>/dev/null
