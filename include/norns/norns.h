6983c1ff5841c000963f0162fbb895a1dd200cbb
Alberto Miranda <alberto.miranda@bsc.es>
2019-03-07 16:05:00 +0100

    Fix formatting

--- include/norns/norns.h
+++ include/norns/norns.h
@@ -84,16 +84,20 @@ norns_iotask_t
 NORNS_IOTASK(norns_op_t operation, norns_resource_t src, ...) __THROW;
 
 /* Submit an asynchronous I/O task */
-norns_error_t norns_submit(norns_iotask_t* task) __THROW;
+norns_error_t 
+norns_submit(norns_iotask_t* task) __THROW;
 
 /* wait for the completion of the I/O task associated to 'task' */
-norns_error_t norns_wait(norns_iotask_t* task) __THROW;
+norns_error_t 
+norns_wait(norns_iotask_t* task) __THROW;
 
 /* Try to cancel an asynchronous I/O task associated with task */
-norns_error_t norns_cancel(norns_iotask_t* task) __THROW;
+norns_error_t 
+norns_cancel(norns_iotask_t* task) __THROW;
 
 /* Check the status of a submitted I/O task */
-norns_error_t norns_status(norns_iotask_t* task, norns_stat_t* stats) __THROW;
+norns_error_t 
+norns_status(norns_iotask_t* task, norns_stat_t* stats) __THROW;
 
 /* Return a string describing the error number */
 char* norns_strerror(norns_error_t errnum) __THROW;
