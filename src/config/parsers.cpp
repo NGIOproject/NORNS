796d94cd7f5ea6dae9985f2fbf67944f5a144fe6
Alberto Miranda <alberto.miranda@bsc.es>
2019-06-03 13:46:23 +0200

    Remove old headers

--- src/config/parsers.cpp
+++ src/config/parsers.cpp
@@ -1,29 +1,3 @@
-/*************************************************************************
- * (C) Copyright 2016-2017 Barcelona Supercomputing Center               *
- *                         Centro Nacional de Supercomputacion           *
- *                                                                       *
- * This file is part of the Echo Filesystem NG.                          *
- *                                                                       *
- * See AUTHORS file in the top level directory for information           *
- * regarding developers and contributors.                                *
- *                                                                       *
- * This library is free software; you can redistribute it and/or         *
- * modify it under the terms of the GNU Lesser General Public            *
- * License as published by the Free Software Foundation; either          *
- * version 3 of the License, or (at your option) any later version.      *
- *                                                                       *
- * The Echo Filesystem NG is distributed in the hope that it will        *
- * be useful, but WITHOUT ANY WARRANTY; without even the implied         *
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR               *
- * PURPOSE.  See the GNU Lesser General Public License for more          *
- * details.                                                              *
- *                                                                       *
- * You should have received a copy of the GNU Lesser General Public      *
- * License along with Echo Filesystem NG; if not, write to the Free      *
- * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.    *
- *                                                                       *
- *************************************************************************/
-
 #include <cstdint>
 #include <string>
 #include <cmath>
