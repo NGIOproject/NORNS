236f9fcaf8e22ba2018d086cfe2d757b439bbec8
Alberto Miranda <alberto.miranda@bsc.es>
2019-06-03 13:44:16 +0200

    Remove old headers

--- src/io/task-manager.cpp
+++ src/io/task-manager.cpp
@@ -1,30 +1,3 @@
-/*************************************************************************
- * Copyright (C) 2017-2018 Barcelona Supercomputing Center               *
- *                         Centro Nacional de Supercomputacion           *
- * All rights reserved.                                                  *
- *                                                                       *
- * This file is part of the NORNS Data Scheduler, a service that allows  *
- * other programs to start, track and manage asynchronous transfers of   *
- * data resources transfers requests between different storage backends. *
- *                                                                       *
- * See AUTHORS file in the top level directory for information           *
- * regarding developers and contributors.                                *
- *                                                                       *
- * The NORNS Data Scheduler is free software: you can redistribute it    *
- * and/or modify it under the terms of the GNU Lesser General Public     *
- * License as published by the Free Software Foundation, either          *
- * version 3 of the License, or (at your option) any later version.      *
- *                                                                       *
- * The NORNS Data Scheduler is distributed in the hope that it will be   *
- * useful, but WITHOUT ANY WARRANTY; without even the implied warranty   *
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  *
- * Lesser General Public License for more details.                       *
- *                                                                       *
- * You should have received a copy of the GNU Lesser General             *
- * Public License along with the NORNS Data Scheduler.  If not, see      *
- * <http://www.gnu.org/licenses/>.                                       *
- *************************************************************************/
-
 #include <boost/optional.hpp>
 #include <limits>
 #include <numeric>
